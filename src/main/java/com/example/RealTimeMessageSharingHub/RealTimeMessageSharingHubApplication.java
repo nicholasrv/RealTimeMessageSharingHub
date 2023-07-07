07107becce1232e18950b8cd7381fac984a9601a
Nicholas Ruas Viegas <nicholasrv@live.com>
2023-07-06 23:09:33 -0300

    setting up repo

---
+++ src/main/java/com/example/RealTimeMessageSharingHub/RealTimeMessageSharingHubApplication.java
@@ -0,0 +1,13 @@
+package com.example.RealTimeMessageSharingHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimeMessageSharingHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealTimeMessageSharingHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/RealTimeMessageSharingHub/RealTimeMessageSharingHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RealTimeMessageSharingHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimeMessageSharingHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
